ed862008459c386c2e360c0492ad4449adbd6402
robinyeon <robin.yeon@gmail.com>
2022-11-17 14:58:50 +0900

    FEAT: 백엔드 ESLint, Prettier 설정

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import sequelize from "./src/config/sequelize";
-import errorMiddleware from "./src/middlewares/error";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import sequelize from './src/config/sequelize';
+import errorMiddleware from './src/middlewares/error';
 
 dotenv.config();
 
@@ -10,12 +10,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 
 sequelize.sync({ force: false });
 
-app.get("/", (req, res, next) => {
-  res.send("Team08 Backend");
+app.get('/', (req, res, next) => {
+  res.send('Team08 Backend');
 });
 
 app.use(errorMiddleware);
