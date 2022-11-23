eb1600df8cc1d12e68987cd717caf49453818e44
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-11-23 15:31:19 +0900

    Feat: userAuthRouter

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -3,7 +3,11 @@ import { userController } from '../controllers/user.ctrl';
 
 const userAuthRouter = Router();
 
-// 회원가입
 userAuthRouter.post('/users', userController.register);
+userAuthRouter.post('/users/login', userController.login);
+userAuthRouter.get('/users', userController.userList);
+userAuthRouter.get('/users/current', userController.current);
+userAuthRouter.put('/users/:userId', userController.edit);
+userAuthRouter.get('/users/:userId', userController.select);
 
 export { userAuthRouter };
