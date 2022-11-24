7bfb37fec0df3b38ae5e7e6e6a800319382d2712
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-11-24 16:06:14 +0900

    feat: user 로그인, 회원가입

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -5,9 +5,10 @@ const userAuthRouter = Router();
 
 userAuthRouter.post('/users', userController.register);
 userAuthRouter.post('/users/login', userController.login);
-userAuthRouter.get('/users', userController.userList);
-userAuthRouter.get('/users/current', userController.current);
-userAuthRouter.put('/users/:userId', userController.edit);
-userAuthRouter.get('/users/:userId', userController.select);
+// userAuthRouter.get('/users', userController.userList);
+
+// userAuthRouter.get('/users/current', userController.current);
+// userAuthRouter.put('/users/:userId', userController.edit);
+// userAuthRouter.get('/users/:userId', userController.select);
 
 export { userAuthRouter };
