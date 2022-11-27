211956cdbc9c62cb92d58518c543f2ca77482338
임동민 <ldm96216@gmail.com>
2022-11-27 13:19:47 +0900

    Feat: 마이페이지 dna결과창 호버시 버튼표시 구현

--- frontend/src/components/mypage/ResultCard.tsx
+++ frontend/src/components/mypage/ResultCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 function ResultCard() {
@@ -6,6 +7,10 @@ function ResultCard() {
       <Img alt="dog_img"></Img>
       <div>강아지이름</div>
       <div>종류 100%</div>
+      <ButtonContainer id="ButtonContainer">
+        <button>상세보기</button>
+        <button>삭제</button>
+      </ButtonContainer>
     </CardContainer>
   );
 }
@@ -19,6 +24,11 @@ const CardContainer = styled.div`
   border: 1px solid black;
   border-radius: 20px;
   padding: 20px;
+
+  :hover #ButtonContainer {
+    // 마우스 호버 시 버튼 표시
+    visibility: visible;
+  }
 `;
 
 const Img = styled.img`
@@ -27,4 +37,9 @@ const Img = styled.img`
   border: 1px solid gray;
   border-radius: 10px;
 `;
+
+const ButtonContainer = styled.div`
+  visibility: hidden;
+`;
+
 export default ResultCard;
