dec53c528af4d6e6add323fde5bbfdf8cf88d07b
soojin <2425sja>
2022-11-28 18:00:16 +0900

    feat: reviewComment_put2

--- backend/src/services/revComment.service.js
+++ backend/src/services/revComment.service.js
@@ -23,6 +23,40 @@ class reviewCommentService {
       return _reviewId;
     }
   }
+
+  static async updateComment({ description, id }) {
+    //db검색
+
+    const descriptionId = await ReviewComment.findOne({
+      where: { id: id },
+    });
+
+    // console.log('1:', descriptionId);
+    // console.log('2:', id);
+    // console.log('3', descriptionId.dataValues.id);
+    // descriptionId가 null인거를 왜 못찾아내는지..?
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!descriptionId) {
+      const errorMessage = '등록한 댓글이 없습니다. 다시 한 번 확인해 주세요.';
+      return errorMessage;
+    }
+
+    // db에 저장
+    if (descriptionId) {
+      const updateComment = await ReviewComment.update(
+        { description: description },
+        { where: { id: id } },
+      );
+      console.log('4:', updateComment);
+      //왜 updateComment는 [1]이 나오는걸까....???
+
+      console.log('5', descriptionId);
+
+      updateComment.errorMessage = null; // 문제 없이 db 저장 완료되었으므로 에러가 없음.
+
+      return descriptionId;
+    }
+  }
 }
 
 export { reviewCommentService };
