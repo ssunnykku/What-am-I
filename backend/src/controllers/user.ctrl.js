392c97394f8a30224a8adf30afa390a696c19dfa
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-11-29 22:42:06 +0900

    Refactor: user.ctrl.js

--- backend/src/controllers/user.ctrl.js
+++ backend/src/controllers/user.ctrl.js
@@ -1,12 +1,10 @@
 import { userService } from '../services/user.service.js';
 import jwt from 'jsonwebtoken';
 import passport from 'passport';
-import { logger } from '../config/winston';
 
 class userController {
   static async register(req, res, next) {
     try {
-      logger.error('POST /users');
       const { nickname, email, password, checkPassword } = req.body;
       const newUser = await userService.addUser({
         nickname,
@@ -25,7 +23,6 @@ class userController {
   }
   static async login(req, res, next) {
     try {
-      // logger.info('POST, /users/login');
       const { email, password } = req.body;
 
       const user = await userService.findUser({ email, password });
@@ -70,7 +67,6 @@ class userController {
 
   static async userList(req, res, next) {
     try {
-      logger.error('GET, /users');
       const users = await userService.users();
       if (users.errorMessage) {
         throw new Error(users.errorMessage);
@@ -83,7 +79,6 @@ class userController {
 
   static async current(req, res, next) {
     try {
-      logger.error('GET, /users/current');
       // jwt 이용 id로 사용자 찾기
       const userId = req.currentUserId;
 
@@ -100,7 +95,6 @@ class userController {
 
   static async edit(req, res, next) {
     try {
-      logger.error('PUT, /users/:userId');
       const userId = req.params.userId;
       console.log(userId);
 
@@ -124,7 +118,6 @@ class userController {
   }
 
   static async setImage(req, res, next) {
-    logger.error('PATCH, /users/:userId/image');
     try {
       const userId = req.params.userId;
       const profileImg = req.file.location;
@@ -151,7 +144,6 @@ class userController {
 
   static async select(req, res, next) {
     try {
-      logger.error('GET, /users/:userId');
       const userId = req.params.userId;
       const findUser = await userService.findUserId({ userId });
       return res.status(200).send(findUser);
