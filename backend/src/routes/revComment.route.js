f24c637676e5f50c74099384daf7d2043648ebff
soojin <2425sja>
2022-12-01 20:54:38 +0900

    주석수정

--- backend/src/routes/revComment.route.js
+++ backend/src/routes/revComment.route.js
@@ -4,24 +4,25 @@ import { loginRequired } from '../middlewares/loginRequired.js';
 
 const reviewCommentAuthRouter = Router();
 
-// 리뷰작성
-
+//댓글쓰기
 reviewCommentAuthRouter.post(
   '/reviewComment/:reviewId',
   loginRequired,
   reviewCommentController.reviewComments,
 );
+//게시물(리뷰)에 댓글 전부 다 보기
 reviewCommentAuthRouter.get(
   '/reviewComment/:reviewId',
   loginRequired,
   reviewCommentController.showComments,
 );
-
+//내가 쓴 리뷰 수정
 reviewCommentAuthRouter.put(
   '/reviewComment/:reviewCommentId',
   loginRequired,
   reviewCommentController.updateComment,
 );
+//내가쓴 댓글 삭제
 reviewCommentAuthRouter.delete(
   '/reviewComment/:reviewCommentId',
   loginRequired,
