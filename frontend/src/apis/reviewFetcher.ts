65137b41b53732695e13e546eecfeee9c3f18dfb
94lfnv <94lfnv@gmail.com>
2022-12-05 10:09:41 +0900

    FEAT: connected review api

--- frontend/src/apis/reviewFetcher.ts
+++ frontend/src/apis/reviewFetcher.ts
@@ -3,24 +3,32 @@ import {
   ReviewInitialType,
 } from '../types/reviewboard/reviewType';
 import axiosInstance from '../utils/axiosInstance';
+import Storage from '../storage/storage';
 
-// 글쓰기
+// 리뷰 게시판 글쓰기
 export async function createReviewRequest(
   endpoint: string,
   { description, images }: ReviewInitialType,
 ) {
-  const formData = new FormData();
-  formData.append('description', description);
-  formData.append('images', images as File);
-  const res = await axiosInstance.post(endpoint, formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
+  // const formData = new FormData();
+  // formData.append('description', description);
+  // formData.append('images', images);
+  // 이미지가 배열이라면 images as File[]로 써야 한다.
+
+  const res = await axiosInstance.post(endpoint, {
+    description: description,
+    images: images,
   });
+
+  // const res = await axiosInstance.post(endpoint, {
+  //   headers: {
+  //     'Content-Type': 'multipart/form-data',
+  //   },
+  // });
   return res.data;
 }
 
-// 댓글 달기
+// 리뷰 창에서 댓글 달기
 export async function createReviewCommentRequest(
   endpoint: string,
   description: ReviewCommentType,
@@ -31,14 +39,60 @@ export async function createReviewCommentRequest(
   return res.data;
 }
 
-// 정보 불러오기
-
+// 리뷰 전체 불러오기
 export async function getReviewsListRequest(endpoint: string) {
+  const res = await axiosInstance.get(endpoint, {
+    headers: {
+      'Content-Type': 'application/json',
+      // Authorization: `Bearer ${Storage.getTokenItem()}`,
+    },
+  });
+  return res.data;
+}
+
+// // 리뷰 하나 클릭하면 하나 가져오기
+export async function getOneReviewRequest(endpoint: string) {
   const res = await axiosInstance.get(endpoint, {
     headers: {
       'Content-Type': 'application/json',
     },
   });
-  console.log(res);
+  return res.data;
+}
+
+// 리뷰 댓글 불러오기
+export async function getReviewCommentsRequest(endpoint: string) {
+  const res = await axiosInstance.get(endpoint, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return res.data;
+}
+
+// 리뷰 수정
+export async function editReviewRequest(endpoint: string, description: string) {
+  const res = await axiosInstance.put(
+    endpoint,
+    {
+      description: description,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Storage.getTokenItem()}`,
+      },
+    },
+  );
+  return res.data;
+}
+
+// 댓글 삭제
+export async function deleteReviewRequest(endpoint: string) {
+  const res = await axiosInstance.delete(endpoint, {
+    headers: {
+      Authorization: `Bearer ${Storage.getTokenItem()}`,
+    },
+  });
   return res.data;
 }
--- frontend/src/types/community/communityType.ts
+++ frontend/src/types/community/communityType.ts
@@ -1,5 +1,7 @@
 export interface CreateCommuInitialType {
   name: string;
-  communityImage?: string | Blob | any;
+  communityImage?: any;
   introduction: string;
 }
+
+// 하나의 값에 url, blob 타입을 받는것 보다 해당 타입을 받을 수 있는 키를 각각 하나씩 쓰는게 낫지 않을까요? 값 사용에 혼란이 초래될 수 있을 것 같습니다.
