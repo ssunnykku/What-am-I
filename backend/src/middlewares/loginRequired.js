9a73a19ad652d03fe1be5754868b3da209fb8d08
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-12-06 21:01:57 +0900

    fix : token

--- backend/src/middlewares/loginRequired.js
+++ backend/src/middlewares/loginRequired.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 function loginRequired(req, res, next) {
-  // const userToken = req.headers['authorization']?.split(' ')[1] ?? 'null';
-  const userToken = token;
+  const userToken = req.headers['authorization']?.split(' ')[1] ?? 'null';
+
   if (userToken === 'null') {
     console.log('서비스 사용 요청이 있습니다.하지만, Authorization 토큰: 없음');
     res.status(400).send('로그인한 유저만 사용할 수 있는 서비스입니다.');
