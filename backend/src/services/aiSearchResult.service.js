9df227267f3e38e6a3c25e717922692df10e0b73
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-12-17 00:19:46 +0900

    주석삭제, 변수명 변경

--- backend/src/services/aiSearchResult.service.js
+++ backend/src/services/aiSearchResult.service.js
@@ -1,49 +1,35 @@
 import { AiSearchResult } from '../models/AiSearchResult.model';
 import { Prediction } from '../models/Prediction.model';
-import { db } from '../models/index';
 
 class aiSearchResultService {
   static async createResult({ aiImage, dogName, userId, predictions }) {
-    // const t = await db.sequelize.transaction();
-
     try {
-      const info = await AiSearchResult.create(
-        {
-          userId,
-          dogName,
-          aiImage,
-        },
-        // { transaction: t },
-      );
-
+      const info = await AiSearchResult.create({
+        userId,
+        dogName,
+        aiImage,
+      });
       const findInfo = await AiSearchResult.findOne({
         where: { userId, aiImage },
       });
 
-      // console.log(findInfo);
-      // throw Error('에러발생');
       const predictResult = [];
       for await (const result of predictions) {
-        const a = await Prediction.create(
-          {
-            aiResultId: findInfo.id,
-            predictId: result.id,
-            label: result.label,
-            score: result.score,
-            rank: result.rank,
-          },
-          // { transaction: t },
-        );
-        predictResult.push(a);
+        const searchResult = await Prediction.create({
+          aiResultId: findInfo.id,
+          predictId: result.id,
+          label: result.label,
+          score: result.score,
+          rank: result.rank,
+        });
+        predictResult.push(searchResult);
       }
 
-      // console.log(predictResult);
       await Promise.all(predictResult); //비동기
-      // await t.commit();
+
       return predictResult;
     } catch (error) {
       console.log(error);
-      // await t.rollback();
     }
   }
 
