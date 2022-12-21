289e08dea4a94f083210f3fe57f4d9e2e3b0f7a3
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-12-21 22:11:55 +0900

    fix: backend http 코드 추가

--- backend/app.js
+++ backend/app.js
@@ -26,53 +26,30 @@ import { aiSearchResultRouter } from './src/routes/aiSearchResult.route';
 import errorMiddleware from './src/middlewares/error';
 
 dotenv.config();
+const app = express();
 
-const app = express(); // https
-// const app2 = express(); // http
+const httpPort = 80;
+const httpsPort = 5001;
 
-const privateKey = fs.readFileSync(process.env.PRIVATEKEY, 'utf8');
-const certificate = fs.readFileSync(process.env.CERTIFICATE, 'utf8');
-const ca = fs.readFileSync(process.env.CA, 'utf8');
+// HTTPS 서버
+https.createServer(credentials, app).listen(process.env.SEVER_PORT, () => {
+  console.log(`HTTPS: Express listening on port ${process.env.SEVER_PORT}`);
+});
+
+// HTTP 서버
+app.listen(process.env.HTTP_PORT, () => {
+  console.log(`HTTP: Express listening on port ${process.env.HTTP_PORT}`);
+});
+
+const privateKey = fs.readFileSync(process.env.PRIVATEKEY);
+const certificate = fs.readFileSync(process.env.CERTIFICATE);
+const ca = fs.readFileSync(process.env.CA);
 
 const credentials = {
   key: privateKey,
   cert: certificate,
   ca: ca,
 };
-
-// const httpServer = http.createServer(app2);
-const httpsServer = https.createServer(credentials, app);
-
-// 80 port -- http
-// app2.get('/', (req, res) => {
-//   console.log('------ http get / -----' + new Date().toLocaleString());
-//   console.log('req.ip => ' + req.ip);
-//   console.log('req.hostname => ' + req.hostname);
-//   console.log(req.url);
-//   console.log(req.originalUrl);
-
-//   res.send('<h1>HTTP Server running on port 80</h1>');
-// });
-
-app.get('/', (req, res) => {
-  console.log('------ https get / -----' + new Date().toLocaleString());
-  console.log('req.ip => ' + req.ip);
-  console.log('req.hostname => ' + req.hostname);
-  console.log(req.url);
-  console.log(req.originalUrl);
-
-  res.send('<h1>HTTPS Server running on port 5001</h1>');
-});
-
-// httpServer.listen(80, () => {
-//   console.log(new Date().toLocaleString());
-//   console.log('HTTP Server running on port 80');
-// });
-
-httpsServer.listen(process.env.SEVER_PORT, () => {
-  console.log(new Date().toLocaleString());
-  console.log(`HTTPS -- listening on port ${process.env.SEVER_PORT} ...`);
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -95,7 +72,3 @@ app.use(communityPostLikeRouter);
 app.use(aiSearchResultRouter);
 
 app.use(errorMiddleware);
-
-// app.listen(process.env.SEVER_PORT, () =>
-//   console.log(`✅ Listening to port 5001`),
-// );
