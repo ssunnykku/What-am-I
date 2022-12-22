6d2cdfaa4a2654bff8dc2c45f0ffad9b5e5f2e27
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-12-22 22:50:57 +0900

    app.js 원복

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,6 @@ import db from './src/models/index';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import sequelize from './src/config/sequelize';
-import http from 'http';
-import https from 'https';
-import fs from 'fs';
 
 //**Router */
 import { communityRouter } from './src/routes/community.route';
@@ -27,52 +24,7 @@ import errorMiddleware from './src/middlewares/error';
 
 dotenv.config();
 
-const app = express(); // https
-// const app2 = express(); // http
-
-const privateKey = fs.readFileSync(process.env.PRIVATEKEY, 'utf8');
-const certificate = fs.readFileSync(process.env.CERTIFICATE, 'utf8');
-const ca = fs.readFileSync(process.env.CA, 'utf8');
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
-
-// const httpServer = http.createServer(app2);
-const httpsServer = https.createServer(credentials, app);
-
-// 80 port -- http
-// app2.get('/', (req, res) => {
-//   console.log('------ http get / -----' + new Date().toLocaleString());
-//   console.log('req.ip => ' + req.ip);
-//   console.log('req.hostname => ' + req.hostname);
-//   console.log(req.url);
-//   console.log(req.originalUrl);
-
-//   res.send('<h1>HTTP Server running on port 80</h1>');
-// });
-
-app.get('/', (req, res) => {
-  console.log('------ https get / -----' + new Date().toLocaleString());
-  console.log('req.ip => ' + req.ip);
-  console.log('req.hostname => ' + req.hostname);
-  console.log(req.url);
-  console.log(req.originalUrl);
-
-  res.send('<h1>HTTPS Server running on port 5001</h1>');
-});
-
-// httpServer.listen(80, () => {
-//   console.log(new Date().toLocaleString());
-//   console.log('HTTP Server running on port 80');
-// });
-
-httpsServer.listen(process.env.SEVER_PORT, () => {
-  console.log(new Date().toLocaleString());
-  console.log(`HTTPS -- listening on port ${process.env.SEVER_PORT} ...`);
-});
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -96,6 +48,6 @@ app.use(aiSearchResultRouter);
 
 app.use(errorMiddleware);
 
-// app.listen(process.env.SEVER_PORT, () =>
-//   console.log(`✅ Listening to port 5001`),
-// );
+app.listen(process.env.SEVER_PORT, () =>
+  console.log(`✅ Listening to port 5001`),
+);
