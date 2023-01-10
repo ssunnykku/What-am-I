d0d7cab0c9e920d0344c88338aefefbd44c06b74
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2023-01-10 16:24:18 +0900

    fix:communityLike.service - count수정

--- backend/src/services/pinnedCommunity.service.js
+++ backend/src/services/pinnedCommunity.service.js
@@ -3,16 +3,18 @@ import { Community } from '../models/Community.model';
 
 class pinnedCommunityService {
   static async pinnedCommunities({ userId, communityId }) {
+    // 조회
     const findCommunity = await PinnedCommunity.findOne({
       where: { userId, communityId },
     });
+    // 내역 없으면 pin 취소하기
     if (findCommunity) {
-      const pin = await PinnedCommunity.destroy({
+      const canceledPin = await PinnedCommunity.destroy({
         where: { userId, communityId },
       });
-
-      return pin;
+      return canceledPin;
     }
+
     const pin = await PinnedCommunity.create({
       userId,
       communityId,
