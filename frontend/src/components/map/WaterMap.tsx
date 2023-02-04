cb0d50583dca2938b3f3ca73546760e0071cbef3
임동민 <ldm96216@gmail.com>
2023-02-04 15:05:49 +0900

    Feat: 지도 마커 클릭 시 중심좌표 이동

--- frontend/src/components/map/WaterMap.tsx
+++ frontend/src/components/map/WaterMap.tsx
@@ -2,6 +2,7 @@
 import SeoulWater from '../../../public/mockdata/SeoulWater.json';
 import { useEffect } from 'react';
 import styled from 'styled-components';
+import { MarkEmailReadSharp } from '@mui/icons-material';
 declare global {
   interface Window {
     kakao: any;
@@ -15,12 +16,14 @@ function WalkMap() {
     let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     let options = {
       //지도를 생성할 때 필요한 기본 옵션
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      center: new window.kakao.maps.LatLng(37.56311, 126.99865), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
 
     let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
     let positions: any = [];
+
     SeoulWater.DATA.map((value) => {
       positions.push({
         content: `<div>${value.cot_conts_name}</div>`,
@@ -28,13 +31,12 @@ function WalkMap() {
       });
     });
 
-    console.log(positions);
-
     for (var i = 0; i < positions.length; i++) {
       // 마커를 생성합니다
       var marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: positions[i].latlng, // 마커의 위치
+        clickable: true,
       });
 
       // 마커에 표시할 인포윈도우를 생성합니다
@@ -55,6 +57,7 @@ function WalkMap() {
         'mouseout',
         makeOutListener(infowindow),
       );
+      kakao.maps.event.addListener(marker, 'click', makeClickListener(marker));
     }
 
     // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
@@ -72,6 +75,16 @@ function WalkMap() {
     function makeOutListener(infowindow: { close: () => void }) {
       return function () {
         infowindow.close();
+      };
+    }
+
+    // 인포윈도우 클릭 클로저를 만드는 함수입니다
+    function makeClickListener(marker: any) {
+      return function () {
+        // 클릭 시 마커 좌표정보 저장
+        var pos = marker.getPosition();
+        // 지도 중심좌표 이동
+        map.panTo(pos);
       };
     }
   }, []);
