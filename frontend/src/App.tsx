70cd99181b72b7e6ba3d28d0bf5f03f403c970cc
94lfnv <94lfnv@gmail.com>
2023-04-07 17:41:28 +0900

    FEAT: socket.io 중간

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import UiErrorFallback from './components/common/UiErrorFallback';
 import MyRouter from './MyRouter';
 import GlobalStyle from './assets/styles/GlobalStyle';
+import { io } from 'socket.io-client';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+// export const socket = io('http://localhost:5001/', {
+//   transports: ['websoket'],
+//   upgrade: false,
+//   forceNew: true,
+// });
 
 function App() {
+  const [response, setResponse] = useState<string>('');
+
+  useEffect(() => {
+    const socket = io(BASE_URL);
+    socket.on('FromAPI', (data) => {
+      console.log(data);
+      // setResponse(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <ErrorBoundary FallbackComponent={UiErrorFallback}>
       <MyRouter />
