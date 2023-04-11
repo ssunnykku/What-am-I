3d94f63de8ab5c00de606808515b6af82b3486e7
94lfnv <94lfnv@gmail.com>
2023-04-11 17:09:40 +0900

    FIX: socket 연결 시도

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import MyRouter from './MyRouter';
 import GlobalStyle from './assets/styles/GlobalStyle';
 import { io } from 'socket.io-client';
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_PUBLIC_URL;
 
 // export const socket = io('http://localhost:5001/', {
 //   transports: ['websoket'],
@@ -14,7 +14,28 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 // });
 
 function App() {
-  // const [response, setResponse] = useState<string>('');
+  const [response, setResponse] = useState<string>('');
+
+  const socket = io('hhttp://127.0.0.1:3500/', {
+    cors: {
+      origin: '*',
+    },
+    transports: ['websocket'],
+  });
+
+  socket.on('test', (socket) => {
+    console.log(socket);
+  });
+
+  const handleRequestSocket = () => {
+    socket.emit('test', {
+      data: 'test socket on client',
+    });
+  };
+
+  function handleChange() {
+    console.log('change handle');
+  }
 
   // useEffect(() => {
   //   const socket = io();
@@ -32,6 +53,11 @@ function App() {
     <ErrorBoundary FallbackComponent={UiErrorFallback}>
       <MyRouter />
       <GlobalStyle />
+      <div>
+        test socket connection
+        <button onClick={handleRequestSocket}>Request</button>
+        <input type="text" onChange={handleChange} />
+      </div>
     </ErrorBoundary>
   );
 }
