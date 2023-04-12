774ff470fca2f4611f2021baa7bb5eb09e1459fb
ssunnykku <ssunnykku@gmail.com>
2023-04-12 17:10:47 +0900

    feat: db 충돌 방지, communityChat 세팅

---
+++ backend/src/services/communityChat.service.js
@@ -0,0 +1,10 @@
+import { CommunityChat } from '../models/CommunityChat.model';
+
+class communityChatService {
+  // 1. 닉네임, 이미지 가져오기
+  static async getUserInfo() {}
+
+  // 2. content 내용 가져오기
+}
+
+export { communityChatService };
