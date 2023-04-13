8d348dd6eed2af8035a4a6b1ae0110c6763925d8
94lfnv <94lfnv@gmail.com>
2023-04-13 13:42:47 +0900

    FEAT: socket 연결 및 css 정리 중

--- frontend/src/components/chat/CommuChatInput.tsx
+++ frontend/src/components/chat/CommuChatInput.tsx
@@ -7,7 +7,7 @@ import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 import { BottomBox, InputBox } from '../../pages/ChatRoomPage';
 import { socketProps } from './CommuChatLog';
 
-const CommuChatInput = ({ nickname, socket }: socketProps) => {
+const CommuChatInput = ({ nickname, profile, socket }: socketProps) => {
   const [chatMessage, setChatMessage] = useState('');
 
   // 여기에 프롭스로 소켓 넘겨주기
@@ -16,6 +16,7 @@ const CommuChatInput = ({ nickname, socket }: socketProps) => {
     socket?.emit('onSend', {
       nickname: nickname,
       msg: chatMessage,
+      profile: profile,
     });
     setChatMessage('');
   };
@@ -44,9 +45,7 @@ const CommuChatInput = ({ nickname, socket }: socketProps) => {
           </div>
           <button
             style={{ width: '10%', marginLeft: '5px' }}
-            // disabled={inputChatMsg.length === 0}
-            // onClick={postChatMsg}
-            // onClick={handleRequestSocket}
+            disabled={chatMessage.length === 0}
           >
             <SendOutlinedIcon
               style={{
@@ -61,44 +60,3 @@ const CommuChatInput = ({ nickname, socket }: socketProps) => {
 };
 
 export default CommuChatInput;
-
-const ChatBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  width: 55vw;
-  height: 95%;
-  max-width: 950px;
-  min-width: 450px;
-  background-color: ${theme.backColor};
-  margin: 10px 0;
-  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.2);
-  border-radius: 5px;
-  font-family: ${font.bold};
-
-  header {
-    padding: 20px 30px;
-    display: flex;
-    align-items: center;
-    border-radius: 20px;
-    border-bottom: solid 0.1px lightgray;
-
-    .chat-image {
-      width: 50px;
-      height: 50px;
-      border-radius: 50%;
-
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        border-radius: 50%;
-      }
-    }
-
-    .chat-name {
-      font-size: 17px;
-      margin-left: 15px;
-    }
-  }
-`;
--- frontend/src/pages/CommuChatPage.tsx
+++ frontend/src/pages/CommuChatPage.tsx
@@ -19,9 +19,11 @@ interface UserInfoType {
 }
 
 const CommuChat = () => {
-  const [nickname, setNickname] = useState('');
-  const [inputChatMsg, setInputChatMsg] = useState<string>('');
+  const [nickname, setNickname] = useState<string>('');
+  const [chatProfile, setChatProfile] = useState<string>('');
   const [commuChatInfo, setCommuChatInfo] = useState<CommunityType>();
+  const [userInfo, setUserInfo] = useState<UserInfoType>();
+  const [inputChatMsg, setInputChatMsg] = useState<string>('');
 
   // 커뮤니티 아이디 가져오기
   let getParameter = (key: string) => {
@@ -78,8 +80,8 @@ const CommuChat = () => {
   };
   const getUserInfo = async () => {
     const res = await getUserData();
-    setNickname(res);
-    console.log(res);
+    setNickname(res.nickname);
+    setChatProfile(res.profileImg);
   };
   useEffect(() => {
     getCommuChatInfo();
@@ -91,7 +93,8 @@ const CommuChat = () => {
   const [currSocket, setCurrSocket] = useState<Socket>();
   const chatUserInfo = {
     roomName: id,
-    userName: nickname,
+    nickname: nickname,
+    profile: chatProfile,
   };
 
   useEffect(() => {
@@ -102,8 +105,6 @@ const CommuChat = () => {
     currSocket.on('connect', () => {
       currSocket.emit('join', chatUserInfo);
     });
-    console.log(chatUserInfo);
-    console.log('커뮤니티 아이디', id);
   }
 
   // socket.on('test', (socket) => {
@@ -138,14 +139,13 @@ const CommuChat = () => {
           <div className="chat-name">{commuChatInfo?.name}</div>
         </header>
         <ContentsBox>
-          {/* <div>
-            test socket connection
-            <button onClick={handleRequestSocket}>Request</button>
-            <input type="text" onChange={handleChange} />
-          </div> */}
           <CommuChatLog socket={currSocket} />
-          <CommuChatInput nickname={nickname} socket={currSocket} />
         </ContentsBox>
+        <CommuChatInput
+          nickname={nickname}
+          profile={chatProfile}
+          socket={currSocket}
+        />
       </ChatBox>
     </BigBox>
   );
@@ -198,4 +198,10 @@ const ContentsBox = styled.div`
   padding: 5px;
   font-family: ${font.normal};
   border-radius: 5px;
+  height: 78%;
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
