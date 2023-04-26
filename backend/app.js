d056eebcfeabdb86539ae0027feff66291b9933c
ssunnykku <ssunnykku@gmail.com>
2023-04-26 15:48:29 +0900

    feat: discord bot ping-pong

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,48 @@ import cors from 'cors';
 import sequelize from './src/config/sequelize';
 import { logger } from './src/config/logger';
 import http from 'http';
-
 import { socketConfig } from './src/config/socket.js';
+import { client } from './src/config/discord.js';
+// import { REST, Routes } from 'discord.js';
+
+// const commands = [
+//   {
+//     name: 'ping',
+//     description: 'Replies with Pong!',
+//   },
+// ];
+
+// const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+// try {
+//   console.log('Started refreshing application (/) commands.');
+
+//   rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
+//     body: commands,
+//   });
+
+//   console.log('Successfully reloaded application (/) commands.');
+// } catch (error) {
+//   console.error(error);
+// }
+
+// import { Client, GatewayIntentBits } from 'discord.js';
+// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
+
+// client.on('interactionCreate', async (interaction) => {
+//   console.log(interaction);
+//   if (!interaction.isChatInputCommand()) return;
+
+//   if (interaction.commandName === 'ping') {
+//     await interaction.reply('Pong!');
+//   }
+// });
+
+// client.login(process.env.DISCORD_TOKEN);
 
 //**Router */
 import { communityRouter } from './src/routes/community.route';
@@ -60,8 +100,9 @@ app.use(communityPostLikeRouter);
 app.use(pinnedCommunityRouter);
 app.use(friendRouter);
 
-app.use(index);
+client;
 
+// app.use(index);
 app.use(errorMiddleware);
 
 app.listen(process.env.SEVER_PORT, () =>
---
+++ backend/src/config/commands/error.js
@@ -0,0 +1,10 @@
+// const { SlashCommandBuilder } = require('discord.js');
+
+// module.exports = {
+//   data: new SlashCommandBuilder()
+//     .setName('error')
+//     .setDescription('Alert Error'),
+//   async execute(interaction) {
+//     await interaction.reply('Pong!');
+//   },
+// };
