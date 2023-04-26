32db10933c4c13058f64a69b64d69eca4ad289ea
ssunnykku <ssunnykku@gmail.com>
2023-04-26 21:19:27 +0900

    feat: discord.js

--- backend/src/config/commands/error.js
+++ backend/src/config/commands/error.js
@@ -1,10 +1,10 @@
-// const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
-// module.exports = {
-//   data: new SlashCommandBuilder()
-//     .setName('error')
-//     .setDescription('Alert Error'),
-//   async execute(interaction) {
-//     await interaction.reply('Pong!');
-//   },
-// };
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('error')
+    .setDescription('Alert Error'),
+  async execute(interaction) {
+    await interaction.reply('Pong!');
+  },
+};
