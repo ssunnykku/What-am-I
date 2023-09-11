4de722ffa4bd8a60b2a6c7d97531833cdff69480
ssunnykku <ssunnykku@gmail.com>
2023-09-11 16:46:42 +0900

    docs: 주석 삭제

--- backend/app.js
+++ backend/app.js
@@ -55,32 +55,6 @@ app.use(communityPostLikeRouter);
 app.use(pinnedCommunityRouter);
 app.use(friendRouter);
 
-// app.get('/api/data', (req, res) => {
-//   // API 요청을 보낼 URL 설정
-//   const apiUrl =
-//     'http://openapi.seoul.go.kr:8088/4371424f4f737375373970566e594d/xml/TbViewGisArisu/1/5/';
-
-//   // Axios를 사용하여 API 요청 보내기
-//   axios
-//     .get(apiUrl)
-//     .then((response) => {
-//       // API 요청이 성공한 경우
-//       const data = response.data;
-
-//       // 받은 데이터를 클라이언트로 전송
-//       res.json(data);
-//     })
-//     .catch((error) => {
-//       // API 요청이 실패한 경우
-//       console.error(error);
-
-//       // 에러 메시지를 클라이언트로 전송
-//       res.status(500).json({ error: 'API 요청에 실패했습니다.' });
-//     });
-// });
-
-// app.use(index);
-// app.use(sendErrorToDiscord);
 app.use(errorMiddleware);
 
 app.listen(process.env.SEVER_PORT, () =>
