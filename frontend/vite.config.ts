7a22353153a12e2e46cd483e56298dcca5bf40e1
ssunnykku <ssunnykku@gmail.com>
2023-09-13 01:14:29 +0900

    fix: frontend html 환경변수 적용(VitePluginHtmlEnv)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    VitePluginHtmlEnv(),
+    react(),
+    tsconfigPaths(),
+    VitePluginHtmlEnv({
+      compiler: true,
+    }),
+  ],
   server: {
     port: 3000,
   },
